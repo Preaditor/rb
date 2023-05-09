fd3b871bc20f3c40444f04d3051113192e697a69
Preaditor <adamdilger@virginmedia.com>
2023-05-09 10:07:03 +0100

    Create echo.bat

---
+++ echo.bat
@@ -0,0 +1,59 @@
+@echo off
+color 04
+title Self Destruct
+echo ==================================================
+echo Self Destruct In 10 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 9 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 8 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 7 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 6 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 5 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 4 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 3 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 2 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 1 Seconds
+echo ==================================================
+timeout 1
+echo ==================================================
+echo Self Destruct In 0 Seconds
+echo ==================================================
+timeout 1
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+goto a
